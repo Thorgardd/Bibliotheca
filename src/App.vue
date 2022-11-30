c5063f7928306568ab0daee10c3dab5cf02aafd4
Thorgardd <rouviller.jeremy59@gmail.com>
2022-11-30 21:23:23 +0100

    Push

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,20 @@
 <template>
-  <Header></Header>
-  <router-view></router-view>
+    <Header></Header>
+    <router-view></router-view>
 </template>
 
 <script>
 import Header from "@/components/layouts/Header";
+
 export default {
-  components: {
-    Header
-  }
+    components: {
+        Header
+    }
 }
 </script>
 
 <style lang="scss">
-*{
+* {
   margin: 0;
   padding: 0;
   box-shadow: none;
